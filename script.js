6a08f6a8f3fc9decdc65d3fc1ae466c702086df9
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2025-10-29 20:00:05 +0100

    style: chenge desing the page game

--- script.js
+++ script.js
@@ -1,104 +1,132 @@
-
-let numbercheck = 0;
-let listCorrect = [];
-const listCard = [];
+let numbercheck ;
+let listCorrect ;
+let listCard ;
 const gameBoard = document.getElementById('gameBoard');
-let a = 0;
-let b = 0;
+let a = -1;
+let b = -1;
 let c = 0;
-let numberCardes = 12;
+let numberCardes ;
+let timer;
+let timeCount = 0;
 
 
-
-for (let i = 1; i <= numberCardes; i++) {
-    gameBoard.innerHTML += `
-    <div class="cader" id= "cader${i}">
-    <div class="card " id="card${i}"></div>
-    </div>
-    `
+function startNewGame() {
+  const levelSelect = document.getElementById("levelSelect").value;
+  numberCardes =parseInt(levelSelect);
+  StartGame();
 }
-const cards = document.querySelectorAll('.card');
+
+function StartGame() {
+  genereTime();
+  gameBoard.innerHTML = "";
+  listCard = [];
+  listCorrect = [];
+  numbercheck = 0;
+  document.querySelector(".moves").innerText = `Moves: 0`;
+
+  for (let i = 1; i <= numberCardes; i++) {
+    gameBoard.innerHTML += `
+      <div class="cader" id="cader${i}">
+        <div class="card">
+          <div class="card-inner" id="card${i}">
+            <div class="card-front"></div>
+            <div class="card-back"></div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  const cardInners = document.querySelectorAll(".card-inner");
+
+  cardInners.forEach(card => {
+    let nImgeA = genereNumber();
+    listCard.push(nImgeA);
+  });
+
+  for (let i = 0; i < listCard.length; i++) {
+    const cardInner = cardInners[i];
+    const back = cardInner.querySelector(".card-back");
+    back.classList.add("img" + listCard[i]);
+    addFlipEvent(cardInner, i);
+  }
+}
+
+function addFlipEvent(cardInner, i) {
+  cardInner.addEventListener("click", function () {
+    if (cheangeBackgrounde2(i, cardInner)) {
+      numbercheck++;
+      document.querySelector(".moves").innerText = `Moves: ${numbercheck}`;
+      cardInner.classList.add("flipped");
+    }
+  });
+}
+
+function cheangeBackgrounde2(i, cardInner) {
+  if (listCorrect.includes(i)) return false;
+  if (i === a) return false;
+
+  if (c === 0) {
+    a = i;
+    c++;
+  } else if (c === 1) {
+    b = i;
+    c++;
+    setTimeout(() => {
+      checkMatch();
+    }, 800);
+  }
+  return true;
+}
+
+function checkMatch() {
+  const cardInners = document.querySelectorAll(".card-inner");
+
+  if (listCard[a] === listCard[b]) {
+    listCorrect.push(a);
+    listCorrect.push(b);
+  } else {
+    cardInners[a].classList.remove("flipped");
+    cardInners[b].classList.remove("flipped");
+  }
+
+  a = -1;
+  b = -1;
+  c = 0;
+  StopedGame();
+}
 
 function genereNumber() {
-    let nImge = Math.floor(Math.random() * numberCardes / 2) + 1;
-    let conture = 0
-    for (let i = 0; i < listCard.length; i++) {
-        if (listCard[i] == nImge) {
-            conture++
-        }
-    }
-    if (conture < 2) {
-        return nImge
-    }
-
-    return genereNumber()
+  let nImge = Math.floor(Math.random() * (numberCardes / 2)) + 1;
+  let count = listCard.filter(x => x === nImge).length;
+  if (count < 2) return nImge;
+  return genereNumber();
 }
 
-cards.forEach(card => {
-    let nImgeA = genereNumber()
-    listCard.push(nImgeA)
-});
-
-
-for (let i = 0; i < listCard.length; i++) {
-    cheangeBackgrounde(cards[i], i)
-}
-function checkIsCorrect(index) {
-    if (listCorrect.includes(index)) {
-    return false
-   }
-  return true
+function genereTime() {
+  clearInterval(timer);
+  timeCount = 0;
+  timer = setInterval(() => {
+    timeCount++;
+    document.querySelector(".timer").innerText = `Time: ${timeCount}s`;
+  }, 1000);
 }
 
-function cheangeBackgrounde2(i) {
-    
-    if (checkIsCorrect(i)) {
-        if (i === a) {
-           return false 
-        }
-        if (c == 2) {
-            if (listCard[a] == listCard[b]) {
-                listCorrect.push(a);
-                listCorrect.push(b)
-                a = 0;
-                b = 0;
-                c = 0;
+function resetGame() {
+  location.reload();
+}
 
-            } else {
-                cards[a].classList.remove('img' + listCard[a])
-                cards[b].classList.remove('img' + listCard[b])
-                a = 0;
-                b = 0;
-                c = 0;
-            }
-        }
-        if (c == 0) {
-            a = i
-            c++
-        } else if (c == 1) {
-            b = i
-            c++
-        }
-        return true
-    }
-    return false
-
-
+function StopedGame() {
+  if (listCorrect.length === numberCardes) {
+    clearInterval(timer);
+    setTimeout(() => {
+      alert(` Game Over!\nMoves: ${numbercheck}\nTime: ${timeCount}s`);
+    }, 500);
+  }
 }
 
 
-function cheangeBackgrounde(card, i) {
 
-    return card.addEventListener("click", function () {
-        let check = cheangeBackgrounde2(i);
-        
-        if (check) {
-            numbercheck++
-            document.getElementsByClassName("moves")[0].innerText = `Moves: ${numbercheck}`;
-            cards[i].classList.add('img' + listCard[i]);
-        }else{
-            alert("the deja saved")
-        }
 
-    })
-}
+
+
